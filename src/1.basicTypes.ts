d6a4b8f37256db10c331c6bb3a7e7cffb2e921f9
Gim YoungPhil <youngphil.gim@gmail.com>
2017-05-23 20:07:51 +0900

    first commit

---
+++ src/1.basicTypes.ts
@@ -0,0 +1,14 @@
+let isDone: boolean = false;
+
+// Error
+// isDone = 10;
+// isDone = 'true';
+
+let isFinish: Boolean = true;
+
+let hexa: number = 0xff;
+let dec: number = 255;
+let octal: number = 0o77;
+let binaay: number = 0b11111111;
+
+console.log(hexa);
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+let test: Object = 4;
+let myNumber: number = 10;
+
+myNumber.toFixed();
